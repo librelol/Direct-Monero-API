db8d9643ab7e99b6fc14f739c8bc2aee1c5abb78
Revolt Hacker <174146808+revolthacker@users.noreply.github.com>
2024-09-23 20:05:09 +0000

    Add initial project files and structure for a vuetify based frontend and fixed error with api dockerfile.

--- web/controllers/mainController.js
+++
@@ -1,8 +0,0 @@
-exports.renderIndex = (req, res) => {
-    res.render('index', {
-        title: 'Welcome to My Website',
-        header: 'Hello, World!',
-        content: 'This is the main content of the page.',
-        footer: '© 2023 My Website'
-    });
-};
