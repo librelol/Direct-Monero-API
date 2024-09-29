02efc842523e1f91254f766c839229972400f3ff
Revolt Hacker <hello@libre.lol>
2024-09-29 18:35:54 -0400

    Removed unused docker compose things that i plan to work on it on a branch. and i began implementing verboselogging to help with debugging eventually

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 require('dotenv').config();
+const verboseLogging = true; // Set to true to enable verbose logging
 
 const connectDB = require('./config/database');
 const { apiLimiter } = require('./middleware/rateLimiter');
@@ -23,12 +24,23 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(apiLimiter);
 
+// Verbose logging middleware
+if (verboseLogging) {
+  app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    console.log('Headers:', req.headers);
+    console.log('Body:', req.body);
+    next();
+  });
+}
+
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/profile', profileRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/post', postRoutes);
 
+// Debug route
 app.use('/api', (req, res) => {
   res.json({ message: 'API is running' });
 });
