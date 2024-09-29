fdfce0892eb85873e41338d484fd78dda4bd892c
Revolt Hacker <hello@libre.lol>
2024-09-29 16:04:42 -0400

    Created page for uploading posts

---
+++ web/src/pages/CreatePost.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-container>
+        <v-row justify="center">
+            <v-col cols="12" md="8">
+                <CreatePost />
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+import CreatePost from '@/components/CreatePost.vue';
+</script>
+
+<style scoped>
+/* Add any scoped styles here */
+</style>
