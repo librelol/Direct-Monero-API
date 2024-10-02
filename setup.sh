bf06c4fbe2fa243ecf6b9abf5cb535ea1d5d9024
Revolt Hacker <hello@libre.lol>
2024-10-02 18:22:20 +0000

    Reorganized project for just the api.

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Ensure the scripts are executable
-chmod +x ./utilities/genenv.sh
-chmod +x ./utilities/start.sh
-
-# Run the generate_environment.sh script
-./utilities/genenv.sh
-./utilities/start.sh
--- utilities/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Change to the parent directory
-cd ..
-
-# Run docker compose up with the --build flag
-docker compose up --build
