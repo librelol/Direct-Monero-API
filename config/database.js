90b33db0eec5a0ba99f0c64184f40348e19827c2
Libre Lol <hello@libre.lol>
2024-10-02 17:41:45 -0400

    Enabled redundancy for db connection retries.

--- config/database.js
+++ config/database.js
@@ -8,20 +8,34 @@ const dbHost = process.env.DB_HOST;
 
 const mongoURI = `mongodb+srv://${dbUser}:${dbPassword}@${dbHost}/${dbName}?authSource=admin`;
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(mongoURI);
-    console.log('MongoDB connected');
+const maxRetries = 5;
+const retryDelay = 30; // 5 seconds
 
-    // Initialize GridFS
-    const conn = mongoose.createConnection(mongoURI);
-    conn.once('open', () => {
-      global.gfs = Grid(conn.db, mongoose.mongo);
-      global.gfs.collection('uploads'); // Set the collection name for GridFS
-    });
-  } catch (err) {
-    console.error('MongoDB connection error:', err);
-    process.exit(1);
+const connectDB = async () => {
+  let retries = 0;
+  while (retries < maxRetries) {
+    try {
+      await mongoose.connect(mongoURI);
+      console.log('MongoDB connected');
+
+      // Initialize GridFS
+      const conn = mongoose.createConnection(mongoURI);
+      conn.once('open', () => {
+        global.gfs = Grid(conn.db, mongoose.mongo);
+        global.gfs.collection('uploads'); // Set the collection name for GridFS
+      });
+      return; // Exit the function if connection is successful
+    } catch (err) {
+      retries += 1;
+      console.error(`MongoDB connection error (attempt ${retries}):`, err);
+      if (retries < maxRetries) {
+        console.log(`Retrying in ${retryDelay / 1000} seconds...`);
+        await new Promise(res => setTimeout(res, retryDelay));
+      } else {
+        console.error('Max retries reached. Exiting...');
+        process.exit(1);
+      }
+    }
   }
 };
 
