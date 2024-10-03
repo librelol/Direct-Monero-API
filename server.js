45920725320747e90ba7c9fefd47fc76d491481e
Libre Lol <hello@libre.lol>
2024-10-03 10:03:04 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const helmet = require('helmet');
 const cors = require('cors');
+const helmet = require('helmet');
 const bodyParser = require('body-parser');
-const apiLimiter = require('./middleware/apiLimiter');
+require('dotenv').config();
+const verboseLogging = true; // Set to true to enable verbose logging
+
 const connectDB = require('./config/database');
+const { apiLimiter } = require('./middleware/rateLimiter');
 const authRoutes = require('./routes/auth');
 const profileRoutes = require('./routes/profile');
 const userRoutes = require('./routes/user');
@@ -19,7 +22,7 @@ connectDB();
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://crypto-market-frontend.vercel.app', 'https://www.directmonero.com', 'https://directmonero.com', 'https://api.directmonero.com'],
+  origin: ['http://localhost:3000', 'https://crypto-market-frontend.vercel.app', 'https://www.directmonero.com', 'https://directmonero.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
@@ -27,7 +30,7 @@ app.use(bodyParser.json());
 app.use(apiLimiter);
 
 // Verbose logging middleware
-if (process.env.VERBOSE_LOGGING === 'true') {
+if (verboseLogging) {
   app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
     console.log('Headers:', req.headers);
@@ -39,10 +42,20 @@ if (process.env.VERBOSE_LOGGING === 'true') {
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/profile', profileRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/posts', postRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/post', postRoutes);
 app.use('/api/admin', adminRoutes); // Use admin routes
 
+// Route to check if the api is working
+app.get('/api', (req, res) => {
+  res.json({ message: 'API is running' });
+});
+
+app.get('/', (req, res) => {
+  res.send('This is the API root. Please make sure you use /api to access the API.');
+});
+
+// Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`API is running on port ${PORT}`);
 });
