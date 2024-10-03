ea5d0f7939defc4818db79f88398080da329612f
Libre Lol <hello@libre.lol>
2024-10-02 20:22:24 -0400

    Allowing for local debugging
    Did this to allow me to speed up the process of dev

--- server.js
+++ server.js
@@ -20,7 +20,11 @@ connectDB();
 
 // Middleware
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://crypto-market-frontend.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(bodyParser.json());
 app.use(apiLimiter);
 
